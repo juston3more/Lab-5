fc691a657b456d7d44448231a8069ab3a60a5221
juston3more <pasaivlev3@gmail.com>
2024-03-16 17:22:38 +0300

    final

--- main-2.py
+++ main-2.py
@@ -1,26 +1,19 @@
-import json
-import requests
+from requests import get
+from datetime import datetime
+from time import sleep
+import os
 
-def get_text():
-    headers = {
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYjJlMTIxYjUtOWJlNy00YjAzLWEzMGYtNjE3YjMxODVlYjRkIiwidHlwZSI6ImFwaV90b2tlbiJ9.lGQpMRulvr5QIAYIIEL4shQhqGtVbN0Zg_0VvJacDvo"}
-
-    url = "https://api.edenai.run/v2/text/generation"
-    payload = {
-        "providers": "openai",
-        "text": "tell me something about space adventures",
-        "temperature": 0.2,
-        "max_tokens": 250
-    }
-
-    response = requests.post(url, json=payload, headers=headers)
-
-    result = json.loads(response.text)
-    print(result)
-    print(result['openai']['generated_text'])
-
-city_name = 'Madrid'
-key = '2f44ab8d94825710392977b37c24d3ac'
-response = requests.post(f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={key}')
-result = json.loads(response.text)
-print(result)
+res = get('http://api.open-notify.org/iss-now.json').json()
+date = datetime.fromtimestamp(res['timestamp']).strftime('%Y-%m-%d %H:%M:%S')
+la = res['iss_position']['latitude']
+lo = res['iss_position']['longitude']
+while True:
+    print('Положение Мкс')
+    print(f'Время: {date}, Широта: {la}, Долгота: {lo}')
+    res = get('http://api.open-notify.org/iss-now.json').json()
+    date = datetime.fromtimestamp(res['timestamp']).strftime('%Y-%m-%d %H:%M:%S')
+    la = res['iss_position']['latitude']
+    lo = res['iss_position']['longitude']
+    sleep(3)
+    os.system('cls')
+    
